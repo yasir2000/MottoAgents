4f09c32c290b152dab0f585c3bc228a71fae2e76
Yasir Karam <yasirkaram@gmail.com>
2025-03-09 23:29:42 +0400

    fixing build stage

--- docker/build.sh
+++ docker/build.sh
@@ -3,4 +3,24 @@
 IMAGE="yasir2000/mottoagents"
 VERSION=1.0
 
-docker build --no-cache -f Dockerfile -t "${IMAGE}:${VERSION}" .
+# Enable BuildKit and set configuration
+export DOCKER_BUILDKIT=1
+export BUILDKIT_PROGRESS=plain
+export BUILDKIT_CONFIG=docker/docker-buildkit.toml
+
+# Enable debug mode for more detailed output
+set -x
+
+# Clean up any existing containers and images
+docker ps -a | grep ${IMAGE} | awk '{print $1}' | xargs -r docker rm -f
+docker images | grep ${IMAGE} | awk '{print $3}' | xargs -r docker rmi -f
+
+# Build with optimizations
+docker build \
+  --progress=plain \
+  --build-arg BUILDKIT_INLINE_CACHE=1 \
+  --build-arg MAKEFLAGS="-j$(nproc)" \
+  --build-arg BUILDKIT_CONTEXT_KEEP_GIT_DIR=1 \
+  -f docker/Dockerfile \
+  -t "${IMAGE}:${VERSION}" \
+  .
